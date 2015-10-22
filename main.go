4c37371502a11deddef603fdb81db21ac56c5d3a
Ricardo Longa <ricardo.longa@gmail.com>
2015-10-21 22:36:14 -0200

    In progress...

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+var pessoas []map[string]interface{}
+
+func main() {
+	pessoas = make([]map[string]interface{}, 0)
+
+	r := gin.Default()
+
+	r.GET("/pessoas", func(c *gin.Context) {
+		c.JSON(200, pessoas)
+	})
+
+	r.POST("/pessoas", func(c *gin.Context) {
+		pessoa := make(map[string]interface{})
+		c.BindJSON(&pessoa)
+
+		pessoas = append(pessoas, pessoa)
+
+		c.AbortWithStatus(http.StatusOK)
+	})
+
+	r.POST("/pessoas/:name", func(c *gin.Context) {
+		name := c.Param("name")
+
+		newCollection := make([]map[string]interface{}, 0)
+
+		for _, obj := range pessoas {
+			if !strings.EqualFold(name, obj["name"].(string)) {
+				newCollection = append(newCollection, obj)
+			}
+		}
+
+		pessoas = newCollection
+
+		c.AbortWithStatus(http.StatusOK)
+	})
+
+	r.Run(":8080")
+}
